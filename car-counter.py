aa62cb4ba71184df316afb0c05a89c306ce7fa53
Arun singh thakur <arunt7767@gmail.com>
2023-12-09 16:26:50 +0530

    Update car-counter.py

--- car-counter.py
+++ car-counter.py
@@ -26,7 +26,10 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
               "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
               "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", 
+              "carrot","hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
               ]
 
 mask = cv2.imread("C:/Users/akash/OneDrive/Desktop/Capstone/Car-Counter-Capstone/mask.png")
